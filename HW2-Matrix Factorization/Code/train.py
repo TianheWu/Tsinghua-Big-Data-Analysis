df361d503b1af6e743ec32cea11166c664f81ff6
TianheWu <tianhe_wu@foxmail.com>
2022-11-22 18:59:51 +0800

    finish cf

--- HW2-Matrix Factorization/Code/train.py
+++ HW2-Matrix Factorization/Code/train.py
@@ -1,53 +1,100 @@
 import numpy as np
+import time
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-
-
-
-
-
-def get_movie_id(movie_data_path):
-    movie_id_list = []
-    with open(movie_data_path, 'r', encoding='ISO-8859-1') as listFile:
+def get_user_movie_score(data_path):
+    user_movie_score = {}
+    with open(data_path, 'r') as listFile:
         for line in listFile:
-            movie_id, year, title = line.split(",", 2)
-            movie_id_list.append(movie_id)
-    return np.array(movie_id_list)
+            user_id, movie_id, score, data_time = line.split()
+            if user_id not in user_movie_score:
+                user_movie_score[user_id] = [(movie_id, float(score))]
+            else:
+                user_movie_score[user_id].append((movie_id, float(score)))
+    return user_movie_score
 
 
-def get_user_id(user_data_path):
-    user_id_list = []
-    with open(user_data_path, 'r') as listFile:
-        for line in listFile:
-            user_id = line[:-1]
-            user_id_list.append(user_id)
-    return np.array(user_id_list)
+def get_score_matrix(user_movie_score, train_data_path, test_data_path):
+    # 将user id与movie id映射到以0开始
+    user_idx, movie_idx = {}, {}
+    count_user, count_movie = 0, 0
+    for key, values in user_movie_score.items():
+        user_idx[key] = count_user
+        for i in range(len(values)):
+            movie_id, score = values[i]
+            if movie_id not in movie_idx:
+                movie_idx[movie_id] = count_movie
+                count_movie += 1
+        count_user += 1
 
-
-def read_train_file(train_data_path):
-    user_movie_score = []
+    # 定义训练集，测试集用户评分矩阵，默认没有评分的位置为0
+    train_score_matrix = np.zeros(shape=(count_user, count_movie))
+    test_score_matrix = np.zeros(shape=(count_user, count_movie))
     with open(train_data_path, 'r') as listFile:
         for line in listFile:
-            user_id, moive_id, score, data_time = line.split()
-            user_movie_score.append([user_id, moive_id, score])
-            
+            user_id, movie_id, score, data_time = line.split()
+            row_idx = user_idx[user_id]
+            column_idx = movie_idx[movie_id]
 
-def get_matrix(user_num, movie_num):
-    score_matrix = np.zeros(shape=(user_num, movie_num))
+            # 将该用户对电影的分数填到对应的位置
+            train_score_matrix[row_idx][column_idx] = float(score)
     
+    with open(test_data_path, 'r') as listFile:
+        for line in listFile:
+            user_id, movie_id, score, data_time = line.split()
+            row_idx = user_idx[user_id]
+            column_idx = movie_idx[movie_id]
+            test_score_matrix[row_idx][column_idx] = float(score)
+
+    return train_score_matrix, test_score_matrix
+
+
+def collaborative_filtering(matrix):
+    # 计算每两个用户之间的余弦相似度，本质就是attn map
+    sim_matrix = cosine_similarity(matrix)
+
+    # 将相似度矩阵与原分数矩阵相乘，矩阵中的每一行为用户i对电影j的预测分数
+    pred_score_matrix = np.dot(sim_matrix, matrix)
+
+    # 再对每一行除以对应的相似度权重
+    weight_sum_vec = sim_matrix.sum(axis=1)
+    weight_sum_vec = weight_sum_vec[:, np.newaxis]
+    weight_sum_vec = weight_sum_vec.repeat([pred_score_matrix.shape[1]], axis=1)
+    pred_score_matrix = pred_score_matrix / weight_sum_vec
+    return pred_score_matrix
+ 
+
+def cal_RMSE(pred_score_matrix, test_score_matrix):
+    """
+    Args:
+        pred_score_matrix ([numpy]): the prediction of score matrix
+        test_score_matrix ([numpy]): test matrix label
+
+    Returns:
+        [float]: the RMSE value
+    """
+    pred = pred_score_matrix[test_score_matrix.nonzero()].flatten()
+    label = test_score_matrix[test_score_matrix.nonzero()].flatten()
+    return sqrt(mean_squared_error(pred, label))
 
 
 if __name__ == "__main__":
-    user_data_path = "./data/users.txt"
     train_data_path = "./data/netflix_train.txt"
-    movie_data_path = "./data/movie_titles.txt"
-    
+    test_data_path = "./data/netflix_test.txt"
 
-    movie_id_list = get_movie_id(movie_data_path)
-    user_id_list = get_user_id(user_data_path)
-    
-    user_num = user_id_list.shape[0]
-    movie_num = movie_id_list.shape[0]
+    # 获取训练集的user和对应的movie id, score
+    train_user_movie_score = get_user_movie_score(data_path=train_data_path)
+    train_score_matrix, test_score_matrix = get_score_matrix(user_movie_score=train_user_movie_score,
+        train_data_path=train_data_path, test_data_path=test_data_path)
 
-    print(user_num)
-    print(movie_num)
+    start_time = time.time()
+    pred_score_matrix = collaborative_filtering(train_score_matrix)
+    algorithm_time = time.time() - start_time
+    rmse = cal_RMSE(pred_score_matrix, test_score_matrix)
+    print("RMSE: {}".format(rmse))
+    print("Collaborative Filtering algorithm time: {}".format(algorithm_time))
+
